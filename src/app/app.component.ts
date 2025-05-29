e29b16a2f3489afb030894ca021f64b42a485557
Stanislaw M. Wasilewski <109121036+gitlaws@users.noreply.github.com>
2025-05-29 18:40:05 -0400

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from "@angular/core";
 import { RouterOutlet } from "@angular/router";
 import { CommonModule } from "@angular/common";
+import { ToolbarComponent } from "./components/toolbar/toolbar.component";
 
 @Component({
   selector: "app-root",
   standalone: true,
-  imports: [RouterOutlet, CommonModule],
+  imports: [RouterOutlet, CommonModule, ToolbarComponent],
   templateUrl: "./app.component.html",
   styleUrls: ["./app.component.scss"],
 })
