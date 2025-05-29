39952b0d0ae9f5a6985b205cfe864a10219780ee
Stanislaw M. Wasilewski <109121036+gitlaws@users.noreply.github.com>
2025-05-29 18:46:22 -0400

    added texttomidi

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,17 @@
-import { Routes } from '@angular/router';
+import { Routes } from "@angular/router";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: "",
+    loadComponent: () =>
+      import("./features/prompt-input/prompt-input.component").then(
+        (m) => m.PromptInputComponent
+      ),
+  },
+
+  //   {
+  //     path: "Home",
+  //     loadComponent: () =>
+  //       import("./core/home/home.component").then((m) => m.HomeComponent),
+  //   },
+];
