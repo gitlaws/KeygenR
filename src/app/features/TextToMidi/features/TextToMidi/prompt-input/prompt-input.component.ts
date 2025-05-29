649ce8d476a4ab8261aba3046cfec663ae9464e9
Stanislaw M. Wasilewski <109121036+gitlaws@users.noreply.github.com>
2025-05-29 18:11:16 -0400

    update default flags

---
+++ src/app/features/TextToMidi/features/TextToMidi/prompt-input/prompt-input.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-prompt-input',
+  standalone: true,
+  imports: [],
+  template: `
+    <p>
+      prompt-input works!
+    </p>
+  `,
+  styles: ``
+})
+export class PromptInputComponent {
+
+}
