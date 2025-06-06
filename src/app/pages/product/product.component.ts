461b5026862b23eb50ebd52663c67cf587713ca0
Stanislaw M. Wasilewski <109121036+gitlaws@users.noreply.github.com>
2025-06-06 11:24:00 -0400

    update

--- src/app/pages/product/product.component.ts
+++ src/app/pages/product/product.component.ts
@@ -1,12 +1,9 @@
-import { CommonModule } from "@angular/common";
 import { Component } from "@angular/core";
-
-import { RouterModule } from "@angular/router";
 
 @Component({
   selector: "app-product",
   standalone: true,
-  imports: [CommonModule, RouterModule],
+  imports: [],
   templateUrl: "./product.component.html",
   styleUrl: "./product.component.scss",
 })
