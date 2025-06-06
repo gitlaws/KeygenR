1ad558c35b83af460b644853b8b341e310bbb403
Stanislaw M. Wasilewski <109121036+gitlaws@users.noreply.github.com>
2025-06-06 11:57:10 -0400

    added click event to nav menu to close while click anywhere

--- src/app/components/nav-menu/nav-menu.component.ts
+++ src/app/components/nav-menu/nav-menu.component.ts
@@ -1,16 +1,51 @@
-import { Component } from "@angular/core";
+import { Component, ElementRef, OnDestroy } from "@angular/core";
 import { RouterLink } from "@angular/router";
+import { CommonModule } from "@angular/common";
 
 @Component({
   selector: "app-nav-menu",
   standalone: true,
-  imports: [RouterLink],
+  imports: [CommonModule, RouterLink],
   templateUrl: "./nav-menu.component.html",
   styleUrl: "./nav-menu.component.scss",
 })
-export class NavMenuComponent {
+export class NavMenuComponent implements OnDestroy {
   menuOpen = false;
-  toggleMenu() {
+  private boundDocumentClickHandler: any;
+
+  constructor(private elementRef: ElementRef) {}
+
+  toggleMenu(event?: MouseEvent) {
+    if (event) event.stopPropagation();
     this.menuOpen = !this.menuOpen;
+
+    if (this.menuOpen) {
+      this.boundDocumentClickHandler = this.documentClickHandler.bind(this);
+      document.addEventListener("click", this.boundDocumentClickHandler);
+    } else {
+      this.removeDocumentClickListener();
+    }
+  }
+
+  closeMenu() {
+    this.menuOpen = false;
+    this.removeDocumentClickListener();
+  }
+
+  private documentClickHandler(event: MouseEvent) {
+    if (!this.elementRef.nativeElement.contains(event.target)) {
+      this.closeMenu();
+    }
+  }
+
+  private removeDocumentClickListener() {
+    if (this.boundDocumentClickHandler) {
+      document.removeEventListener("click", this.boundDocumentClickHandler);
+      this.boundDocumentClickHandler = null;
+    }
+  }
+
+  ngOnDestroy(): void {
+    this.removeDocumentClickListener();
   }
 }
